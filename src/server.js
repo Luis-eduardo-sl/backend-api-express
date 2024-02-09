86d70f39c1369542e7a3878a5fd6efb9a4f20167
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-02-09 15:19:44 -0300

    config

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 // const express = require('express')
 import express from 'express'
+import {PORT, HOST} from './config.js'
+
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/', (req, res) => {
+  res.send('Hello World! post')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${HOST}:${PORT}`)
 })
