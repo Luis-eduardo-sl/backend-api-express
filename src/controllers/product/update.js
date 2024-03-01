57545f04b283365e9df961e11e2c99fcc3df818e
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-01 16:29:46 -0300

    router update for product with prisma

--- src/controllers/product/update.js
+++ src/controllers/product/update.js
@@ -1,5 +1,20 @@
-const update = (req, res) =>{
-    res.json({message: 'Está é a rota put /product/'})
-}
+import productModel from "../../models/productModel.js";
+
+const update = async (req, res) =>{
+    try{
+        const id = req.params.id
+        const product = await productModel.update(+id, req.body)
+        
+        return res.json({
+            sucess: 'Produto atualizado com sucesso!',
+            product
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            error: 'Opss erro no servidor, tente novamente!'
+        })
+    }
+};
 
 export default update
