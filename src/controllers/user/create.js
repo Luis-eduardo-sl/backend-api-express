2d6353f771fdaccffab2be4420be381a40583875
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-01 17:57:26 -0300

    validation with zod

--- src/controllers/user/create.js
+++ src/controllers/user/create.js
@@ -1,12 +1,27 @@
 import userModel from "../../models/userModel.js";
+import zodErrorFormat from "../../helpers/zodErrorFormat.js"
 
 const create = async (req, res) =>{
 
     try{
-        const users = await userModel.create(req.body)
+        const result = userModel.validateUserToCreat(req.body)
+
+        if(!result.success){
+            // const errorFormated = result.error.format()
+            // delete errorFormated._errors 
+            // errorFormated.name ? errorFormated.name = errorFormated.name._errors : null
+            // errorFormated.email ? errorFormated.email = errorFormated.email._errors : null
+            // errorFormated.avatar ? errorFormated.avatar = errorFormated.avatar._errors : null
+            return res.status(400).json({
+                error: 'Dados de cadastro invalido',
+                fields: zodErrorFormat(result.error)
+            })
+        }
+        const user = await userModel.create(req.body)
+
         return res.json({
             sucess: 'Usuarios criado com sucesso!',
-            users
+            user
         })
     } catch (error) {
         console.log(error);
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,37 @@
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const prisma = new PrismaClient()
+
+const userSchema = z.object({
+    id: z.number({
+        required_error: "ID é obrigatorio",
+        invalid_type_error: "O ID deve ser um numero inteiro"
+    }) ,
+    name: z.string({
+        required_error: "Nome é obrigatorio",
+        invalid_type_error: "o nome deve ser uma string"
+    })
+    .min(3,{message: "o nome deve ter no minimo 3 letras"})
+    .max(200,{message: "o nome deve ter no maximio 200 caracteres"}) ,
+    email: z.string({
+        required_error: "O email é obrigatorio",
+        invalid_type_error: "o email deve ser uma string"
+    })
+    .email({message: "Email inválido"})
+    .max(500, {message: "O email deve ter no maximo 500 caracteres"}) ,
+    avatar: z.string({
+        required_error: "O avatar é obrigatorio",
+        invalid_type_error: "O avatar deve ser uma string"
+    })
+    .url({message: "URL do avatar invalida"})
+    .max(1000 , {message: "URL deve ter no maximo 1000 caracteres"})
+})
+
+const validateUserToCreat = (user) => {
+    const partialUserSchema = userSchema.partial({id: true})
+    return partialUserSchema.safeParse(user)
+}
 
 const getAll = async () =>{
     return await prisma.user.findMany()
@@ -31,4 +62,4 @@ const update = async (id, user) =>{
 })
 }
 
-export default {getAll, getById, create, remove, update}
+export default {getAll, getById, create, remove, update, validateUserToCreat}
