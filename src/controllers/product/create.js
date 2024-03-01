dbfc969cf000f47a68d1cd322745278da491e34f
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-01 16:29:03 -0300

    router create for product with prisma

--- src/controllers/product/create.js
+++ src/controllers/product/create.js
@@ -1,6 +1,19 @@
-const create = (req, res) =>{
-    const product = req.body
-    res.json({message: 'Está é a rota POST /product/'})
-}
+import productModel from "../../models/productModel.js";
+
+const create = async (req, res) =>{
+
+    try{
+        const products = await productModel.create(req.body)
+        return res.json({
+            sucess: 'Produto criado com sucesso!',
+            products
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            error: 'Opss erro no servidor, tente novamente!'
+        })
+    }
+};
 
 export default create
