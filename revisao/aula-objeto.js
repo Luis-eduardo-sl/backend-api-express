8a69020ed4d46d3c7f62ae62e2133cd432003ec6
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-01 16:33:28 -0300

    revisao aula obj

--- revisao/aula-objeto.js
+++ revisao/aula-objeto.js
@@ -1,21 +1,64 @@
 const endereco = {
-    rua: 'Rua dos bobos',
+    rua: 'Rua dos Bobos',
     numero: 0
 }
 
-
 const user = {
-    name: 'luis',
-    idade: 23,
-    email: 'luis@DFssg.vovm',
-    cidade: 'São Paulo',
+    name: 'Renan',
+    idade: 39,
+    email: 'renancavichi@gmail.com',
+    cidade: 'Caragua',
     prof: true,
     end: endereco,
-    familia:{
+    familia: {
         mae: 'Maria',
-        pai: 'José'
-    }
+        pai: 'João',
+        irmao: 'José'
+    },
+    pets: [{nome: 'Tobias', raca: 'viralata'}, 'Gato', 'Peixe']
+} 
+
+const obj1 = {
+    prop1: 1,
+    prop2: 2
 }
 
+const obj2 = {
+    prop3: 3,
+    prop4: 4
+}
 
-console.log(user)
+//spread ou espalhamento
+const tudo = {...obj1, ...obj2}
+// {prop1: 1, prop2: 2, prop3: 3, prop4: 4}
+ 
+// const prop1 = tudo.prop1
+// const prop2 = tudo.prop2
+// const prop3 = tudo.prop3
+// const prop4 = tudo.prop4
+
+//Atribuição via desestruturação (destructuring assignment)
+const array1 = [10, 'ola', 'teste', {prop1: 'oi'}]
+
+//desestruturar array pela posição, separando por virgula
+const [idade, , ,teste] = array1
+
+console.log('TESTE: ', idade, prop1)
+ 
+//const {prop1, prop2, prop3, prop4} = tudo 
+
+
+// const exemplo = (props) => {
+//     console.log(props.prop1, props.prop2, props.prop3, props.prop4)
+// }
+
+// const exemplo = (props) => {
+//     const {prop1, prop2, prop3, prop4} = props
+//     console.log(prop1, prop2, prop3, prop4)
+// }
+
+const exemplo = ({prop1, prop2, prop3, prop4}) => {
+    console.log(prop1, prop2, prop3, prop4)
+}
+
+console.log(prop1)
