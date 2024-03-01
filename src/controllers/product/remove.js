e1eefc39f2fc70079ab0560c804d5f155db6e5a6
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-01 16:29:37 -0300

    router remove for product with prisma

--- src/controllers/product/remove.js
+++ src/controllers/product/remove.js
@@ -1,6 +1,20 @@
-const remove = (req, res) =>{
-    const id = req.params.id
-    res.json({message: 'Está é a rota Delete /product/:id ' + 'ID = ' +id })
-}
+import productModel from "../../models/productModel.js";
+
+const remove = async (req, res) =>{
+    try{
+        const id = req.params.id
+        const product = await productModel.remove(+id)
+        
+        return res.json({
+            sucess: 'Produto apagado com sucesso!',
+            product
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            error: 'Opss erro no servidor, tente novamente!'
+        })
+    }
+};
 
 export default remove
