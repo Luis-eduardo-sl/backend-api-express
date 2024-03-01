d93804f1e3160895a571816a0abebef34cd5ed9a
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-01 16:29:28 -0300

    router listAll for product with prisma

--- src/controllers/product/listAll.js
+++ src/controllers/product/listAll.js
@@ -1,5 +1,20 @@
-const listAll =  (req, res) =>{
-    res.json({message: 'Está é a rota /product/'})
+import productModel from "../../models/productModel.js";
+
+const listAll = async (req, res) =>{
+
+    try{
+        const product = await productModel.getAll()
+        return res.json({
+            sucess: 'Produto listados com sucesso!',
+            product
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            error: 'Opss erro no servidor, tente novamente!',
+            product
+        })
+    }
 };
 
 export default listAll
