785d1d531f5b5a70bb9b29fce503c60d62c9cff7
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-22 14:56:44 -0300

    add cors , and config

--- src/server.js
+++ src/server.js
@@ -9,6 +9,11 @@ const app = express()
 
 app.use(logger)
 app.use(express.json())
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:8081', 'http://meusite.com'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use('/user', userRouter)
 app.use('/product', productRouter)
