cf6400cc300b8698561c993baaef8340cf09f639
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-04-26 16:10:43 -0300

    add token jwt generate

--- src/controllers/product/update.js
+++ src/controllers/product/update.js
@@ -1,20 +1,27 @@
-import productModel from "../../models/productModel.js";
+import userModel from "../../models/userModel.js"
 
-const update = async (req, res) =>{
-    try{
-        const id = req.params.id
-        const product = await productModel.update(+id, req.body)
-        
-        return res.json({
-            sucess: 'Produto atualizado com sucesso!',
-            product
+const update = async (req, res) => {
+    try{ 
+        const user = req.body
+        user.id = +req.params.id
+        const result = userModel.validateUserToUpdate(user)
+        if(!result.success){
+            return res.status(400).json({
+                error: `Dados de Atualização Inválido`,
+                fields: zodErrorFormat(result.error)
+            })
+        }
+        const userEdited = await userModel.edit(user)
+        res.json({
+            success: `Usuário ${userEdited.id} editado com sucesso!`,
+            user: userEdited
         })
     } catch (error) {
-        console.log(error);
+        console.log(error)
         return res.status(500).json({
-            error: 'Opss erro no servidor, tente novamente!'
+            error: 'Opsss erro no servidor, tente novamente!'
         })
     }
-};
+}
 
 export default update
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,75 +1,126 @@
 import { PrismaClient } from '@prisma/client'
-import { z } from 'zod'
+import { z } from "zod"
 
 const prisma = new PrismaClient()
 
 const userSchema = z.object({
     id: z.number({
-        required_error: "ID é obrigatorio",
-        invalid_type_error: "O ID deve ser um numero inteiro"
-    }) ,
+        required_error: "ID é obrigatório.",
+        invalid_type_error: "O ID deve ser um número inteiro.",
+      }),
     name: z.string({
-        required_error: "Nome é obrigatorio",
-        invalid_type_error: "o nome deve ser uma string"
-    })
-    .min(3,{message: "o nome deve ter no minimo 3 letras"})
-    .max(200,{message: "o nome deve ter no maximio 200 caracteres"}) ,
+        required_error: "Nome é obrigatório.",
+        invalid_type_error: "O nome deve ser uma string.",
+      })
+      .min(3, {message: 'O nome deve ter no mínimo 3 letras.'})
+      .max(200, {message: 'O nome deve ter no máximo 200 caracteres.'}),
     email: z.string({
-        required_error: "O email é obrigatorio",
-        invalid_type_error: "o email deve ser uma string"
-    })
-    .email({message: "Email inválido"})
-    .max(500, {message: "O email deve ter no maximo 500 caracteres"}) ,
+        required_error: "O email é obrigatório.",
+        invalid_type_error: "O email deve ser uma string.",
+      })
+      .email({message: 'Email inválido.'})
+      .max(500, {message: 'O email deve ter no máximo 500 caracteres.'}),
     pass: z.string({
-        required_error: "A senha deve ser uma string",
-        invalid_type_error: " A senha deve ser uma string"
-    })
-    .min(6, {message: "A senha deve ter no minimo 6 caracteres"}),
+        required_error: "A senha é obrigatória.",
+        invalid_type_error: "A senha deve ser uma string.",
+      })
+      .min(6, {message: 'A senha deve ter no mínimo 6 caracteres.'}),
     avatar: z.string({
-        required_error: "O avatar é obrigatorio",
-        invalid_type_error: "O avatar deve ser uma string"
-    })
-    .url({message: "URL do avatar invalida"})
-    .max(1000 , {message: "URL deve ter no maximo 1000 caracteres"})
+        required_error: "O avatar é obrigatório.",
+        invalid_type_error: "O avatar deve ser uma string.",
+      })
+      .url({message: 'Url do avatar inválida.'})
+      .max(1000, {message: 'O avatar deve ter no máximo 1000 caracteres.'})
 })
 
-const validateUserToCreat = (user) => {
+const validateUserToCreate = (user) => {
     const partialUserSchema = userSchema.partial({id: true})
     return partialUserSchema.safeParse(user)
 }
 
-const getAll = async () =>{
-    return await prisma.user.findMany()
+const validateUserToUpdate = (user) => {
+    const partialUserSchema = userSchema.partial({pass: true})
+    return partialUserSchema.safeParse(user)
 }
 
-const create = async (user) =>{
-    return await prisma.user.create({
-        data: user,
-        select:{
+const validateUserToLogin = (user) => {
+    const partialUserSchema = userSchema.partial({id: true, name: true, avatar: true})
+    return partialUserSchema.safeParse(user)
+}
+
+const getAll = async () => {
+    return await prisma.user.findMany({
+        select: {
+            id: true,
             name: true,
             email: true,
-            avatar: true,
+            avatar: true
         }
     })
 }
 
-const getById = async (id) =>{
-    return await prisma.user.findUnique({where: {
-        id
-    }})
+const getById = async (id) => {
+    return await prisma.user.findUnique({
+        where: {
+            id
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
+    })
 }
 
-const remove = async (id) =>{
-    return await prisma.user.delete({where: {
-        id
-    }})
+const getByEmail = async (email) => {
+    return await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
 }
 
-const update = async (id, user) =>{
-    return await prisma.user.update({where: {
-        id,
-    } ,data: user
-})
+const create = async (user) => {
+    return await prisma.user.create({
+        data: user,
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
+    })
 }
 
-export default {getAll, getById, create, remove, update, validateUserToCreat}
+const remove = async (id) => {
+    return await prisma.user.delete({
+        where: {
+            id
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
+    })
+}
+
+const edit = async (user) => {
+    return await prisma.user.update({
+        where: {
+            id: user.id
+        },
+        data: user,
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
+    })
+}
+
+
+export default {getAll, getById, getByEmail, create, remove, edit, validateUserToCreate, validateUserToUpdate, validateUserToLogin}
